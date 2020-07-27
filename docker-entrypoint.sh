3e8994dd4c0f983d2a3eab038564fafbfbef3017
DyonR <DyonR@users.noreply.github.com>
2020-07-27 02:27:47 +0200

    Added GID and adjused chmod/chown for it

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # only add user if not already existent, otherwise container wont restart
 if [ $(getent passwd occlient|wc -l) -eq 0 ]; then
-	useradd --uid $RUN_UID -m --shell /bin/bash occlient
+	useradd --uid $RUN_UID --gid $RUN_GID -m --shell /bin/bash occlient
 fi
 
 netrc_file="/home/occlient/.netrc"
@@ -13,9 +13,9 @@ machine $oc_server_without_port
 	login $OC_USER
 	password $OC_PASS
 EOF
-chown occlient $netrc_file
-chmod 600 $netrc_file
+chown $RUN_UID:$RUN_GID $netrc_file
+chmod $RUN_UID:$RUN_GID $netrc_file
 
-chown -R occlient /ocdata
+chown -R $RUN_UID:$RUN_GID /ocdata
 
 exec $@
